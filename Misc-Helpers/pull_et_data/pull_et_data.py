7dc330fb026a000e6799f574270e03136a39369c
Robin <sifre002$umn.edu>
2020-07-21 09:31:43 -0500

    create log of which files were copied/which directories already existed

--- Misc-Helpers/pull_et_data/pull_et_data.py
+++ Misc-Helpers/pull_et_data/pull_et_data.py
@@ -2,6 +2,7 @@ import argparse
 import shutil
 import os
 from pathlib import Path
+from datetime import datetime
 
 '''configuration: pull_et_data.py --rootdir
 /Users/sifre002/Box/Elab_ET_Data/BCP_BSLERP/AUDIT_PASSED/
@@ -10,7 +11,19 @@ from pathlib import Path
 --tasks
 Calibration_Verification,EU-AIMS_counter_1 '''
 
-'''  
+'''  def getListOfFiles(dirname):
+    # create list of files
+    file_list = os.listdir(dirname)
+    all_files = list()
+    #all_subdirs = list()
+    for entry in file_list:
+        full_path = os.path.join(dirname, entry)
+        if os.path.isdir(full_path): # If full_path is a directory
+            all_files=all_files + getListOfFiles(full_path) # get file list from directory
+        else:
+            all_files.append( os.path.relpath(full_path, dirname) )
+    return all_files
+
 def getListOfSubDirs(dirname):
     # create list of subdirs
     file_list = os.listdir(dirname)
@@ -23,25 +36,39 @@ def getListOfSubDirs(dirname):
     return all_subdirs '''
 
 def main(rootdir, destination, tasks):
-    # Copy specific eye-tracking tasks
+    # Copy specific eye-tracking tasks from rootdir to destination
+
+
 
     # Create the destination directory for all ET files if it does not exist
     if not os.path.exists(destination):
         os.mkdir(destination)
 
-    tasks=tasks.split(',')
+    # start log of files with issues
+    file_log = open(os.path.join(destination, "log.txt"), 'a+')
+    print("---------------- " + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " ----------------",
+          file=file_log)
 
-    # Go thru participant files & look for tasks
-    for p in os.listdir(rootdir): # For each participant directory
+    tasks=tasks.split(',')
+    # Go thru participant files in ROOTDIR & look for tasks
+    for p in os.listdir(rootdir): # For each participant directory:
         partic_dir=os.path.join(rootdir, p)
-        for dirs in os.listdir(partic_dir): # For each visit-directories,select the task directories to copy
-            task_dirs = os.listdir(os.path.join(partic_dir,dirs))
-            task_dirs[:] = [x for x in task_dirs if x in tasks]
+        for dirs in os.listdir(partic_dir): # For each visit-directories:
+            # select the task directories to copy
+            task_dirs = os.listdir(os.path.join(partic_dir,dirs)) # List task directories
+            task_dirs[:] = [x for x in task_dirs if x in tasks] # Reduce TASK_DIRS to include TASKS that user queries
             for t in task_dirs:
                 file_to_copy = os.path.join(partic_dir, dirs, t)
                 path = Path(file_to_copy) # Get the visit folder ID
                 visit_dir = os.path.basename(path.parent)
-                shutil.copytree(src=file_to_copy, dst=os.path.join(destination, p, visit_dir, t))
+                dest_dir = os.path.join(destination, p, visit_dir, t)
+
+                # Copy the task directory, if it does not exist
+                if not os.path.exists(dest_dir):
+                    shutil.copytree(src=file_to_copy, dst=dest_dir)
+                    print(os.path.relpath(file_to_copy, rootdir) + ",Copied",  file=file_log)
+                else:
+                    print(os.path.relpath(file_to_copy, rootdir) + ",Skipped",  file=file_log)
 
 
 
