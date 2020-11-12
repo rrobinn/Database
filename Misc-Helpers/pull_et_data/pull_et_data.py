cde849c8d179294e26eac90a44dd8e7a0aeed853
Robin <sifre002$umn.edu>
2020-11-12 08:19:09 -0600

    add option for re-organizing from BIDS format to a separate directory for each session

--- Misc-Helpers/pull_et_data/pull_et_data.py
+++ Misc-Helpers/pull_et_data/pull_et_data.py
@@ -3,7 +3,7 @@ import shutil
 import os
 from pathlib import Path
 from datetime import datetime
-
+import re
 '''configuration: pull_et_data.py --rootdir
 /Users/sifre002/Box/Elab_ET_Data/BCP_BSLERP/AUDIT_PASSED/
 --destination
@@ -35,11 +35,8 @@ def getListOfSubDirs(dirname):
             all_subdirs=all_subdirs + getListOfSubDirs(full_path) # Recursively check if there is a sub-dir
     return all_subdirs '''
 
-def main(rootdir, destination, tasks):
+def main(rootdir, destination, tasks, keepBIDS=True):
     # Copy specific eye-tracking tasks from rootdir to destination
-
-
-
     # Create the destination directory for all ET files if it does not exist
     if not os.path.exists(destination):
         os.mkdir(destination)
@@ -67,20 +64,33 @@ def main(rootdir, destination, tasks):
             if not task_dirs: # If the list is empty
                 print(os.path.relpath(partic_dir, rootdir) + ",Does not have tasks in subdirectories", file=file_log)
             else:
-                for t in task_dirs:
-                    file_to_copy = os.path.join(partic_dir, dirs, t)
-                    path = Path(file_to_copy) # Get the visit folder ID
-                    visit_dir = os.path.basename(path.parent)
-                    dest_dir = os.path.join(destination, p, visit_dir, t)
-
-                    # Copy the task directory, if it does not exist
-                    if not os.path.exists(dest_dir):
-                        shutil.copytree(src=file_to_copy, dst=dest_dir)
-                        print(os.path.relpath(file_to_copy, rootdir) + ",Copied",  file=file_log)
-                    else:
-                        print(os.path.relpath(file_to_copy, rootdir) + ",Skipped",  file=file_log)
-
-
+                for t in task_dirs: # For each task sub-directory
+                    dir_to_copy = os.path.join(partic_dir, dirs, t)
+                    visit_path = Path(dir_to_copy)  # Get the visit folder ID
+                    visit_dir = os.path.basename(visit_path.parent)
+                    if keepBIDS is True:
+                        dest_dir = os.path.join(destination, p, visit_dir, t)
+                        # Copy the task directory, if it does not exist
+                        if not os.path.exists(dest_dir):
+                            shutil.copytree(src=dir_to_copy, dst=dest_dir)
+                            print(os.path.relpath(dir_to_copy, rootdir) + ",Copied",  file=file_log)
+                        else:
+                            print(os.path.relpath(dir_to_copy, rootdir) + ",Skipped",  file=file_log)
+                    else: # Re-organize so each session is its own directory
+                        # Create destination directory for this visit
+                        visit_num = re.sub("[^0-9]", "", visit_dir)
+                        dest_dir = os.path.join(destination, p+"_"+visit_num)
+                        # Create folder for visit if it does not exist
+                        if not os.path.exists(dest_dir):
+                            os.mkdir(dest_dir)
+                        files_to_copy = os.listdir(dir_to_copy)
+                        for f in files_to_copy:
+                            task_file = dir_to_copy+"/"+f
+                            if not os.path.exists(task_file):
+                                shutil.copy(src=dir_to_copy+"/"+f, dst=dest_dir)
+                                print(os.path.relpath(task_file, rootdir) + ",Copied", file=file_log)
+                            else:
+                                print(os.path.relpath(task_file, rootdir) + ",Skipped", file=file_log)
 
 if __name__ == '__main__':
     ap=argparse.ArgumentParser(
@@ -89,9 +99,10 @@ if __name__ == '__main__':
                     help="path to parent directory for project containing eye tracking files")
     ap.add_argument("--destination", required=False, type=str,
                     help="Where data should be copied to")
-
     ap.add_argument("--tasks", required=False, type=str,
                     help="e.g. Calibration_Verification, EU-AIMS_counter_1, EU-AIMS_counter_2")
+    ap.add_argument("--keepBIDS", required=False, type=bool,
+                    help="Keep BIDS format? (Default=True. If False, then each visit gets its own folder")
 
     args=ap.parse_args()
     main(args.rootdir, args.destination, args.tasks)
